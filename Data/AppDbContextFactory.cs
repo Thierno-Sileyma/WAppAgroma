7d6b1950786fc0c5084bfa216e94cbcd621b3673
Thierno-Sileyma <male.thierno3@gmail.com>
2025-01-11 15:18:48 -0500

    Janvier11

--- Data/AppDbContextFactory.cs
+++ Data/AppDbContextFactory.cs
@@ -1,6 +1,29 @@
-﻿namespace WAppAgroma.Properties
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+using WAppAgroma.Data;
+
+namespace WAppAgroma
 {
-    public class AppDbContext
+    public class AppDbFactory : IDesignTimeDbContextFactory<AppDbContext>
     {
+        public AppDbContext CreateDbContext(string[] args)
+        {
+            // Charger la configuration à partir de appsettings.json
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            // Récupérer la chaîne de connexion
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+            // Configurer les options de DbContext
+            var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
+            optionsBuilder.UseSqlServer(connectionString);
+
+            return new AppDbContext(optionsBuilder.Options);
+        }
     }
 }
